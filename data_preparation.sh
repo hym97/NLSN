b93ab92ba7d74bf8d3e1b5643f44294474162a9b
hym97 <78656286+hym97@users.noreply.github.com>
2023-01-22 23:03:00 -0500

    1/22 data_preparation.sh generation

---
+++ data_preparation.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if command -v python3.9 &>/dev/null; then
+  echo "Python 3.9 is already installed."
+else
+  # Add the repository for Python 3.9
+  sudo add-apt-repository -y ppa:deadsnakes/ppa
+  # Update package lists
+  sudo apt-get update
+  # Install Python 3.9
+  sudo apt-get install python3.9 -y
+  # Check if Python 3.9 is installed correctly
+fi
+
+if [[ -d "envs/NLSN" ]]; then
+  echo "Virtual environment 'myenv' exists."
+  source NLSN/bin/activate
+else
+  echo "Virtual environment 'myenv' does not exist."
+  python3.9 -m venv NLSN
+  source NLSN/bin/activate
+  pip install super-image
+  pip install datasets
+fi
+
+python3.9 test.py
---
+++ test.py
@@ -0,0 +1,31 @@
+from super_image import Trainer, TrainingArguments, EdsrModel, EdsrConfig
+from datasets import load_dataset
+from super_image.data import EvalDataset, TrainDataset, augment_five_crop
+import os
+
+curr_dir = os.getcwd()
+augmented_dataset = load_dataset('eugenesiow/Div2k', 'bicubic_x4', split='train', path=curr_dir)\
+    .map(augment_five_crop, batched=True, desc="Augmenting Dataset")                                # download and augment the data with the five_crop method
+train_dataset = TrainDataset(augmented_dataset)                                                     # prepare the train dataset for loading PyTorch DataLoader
+eval_dataset = EvalDataset(load_dataset('eugenesiow/Div2k', 'bicubic_x4', split='validation', path=curr_dir))      # prepare the eval dataset for the PyTorch DataLoader
+
+#
+#
+# training_args = TrainingArguments(
+#     output_dir='./results',                 # output directory
+#     num_train_epochs=1000,                  # total number of training epochs
+# )
+#
+# config = EdsrConfig(
+#     scale=4,                                # train a model to upscale 4x
+# )
+# model = EdsrModel(config)
+#
+# trainer = Trainer(
+#     model=model,                         # the instantiated model to be trained
+#     args=training_args,                  # training arguments, defined above
+#     train_dataset=train_dataset,         # training dataset
+#     eval_dataset=eval_dataset            # evaluation dataset
+# )
+#
+# trainer.train()
